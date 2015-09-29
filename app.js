98152e9fd0bd10ef87572fdcf1fa4ae5c4425b0c
sfmqur <sammaruska@gmail.com>
2015-09-28 21:19:03 -0500

    migrated to ejs and established routes

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -37,7 +37,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function(err, req, res) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -48,7 +48,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res) {
   res.render('index');
 });
 
+//get ask page
+router.get('/ask', function(req,res){
+  res.render('ask');
+});
+
+//get done page
+router.get('/done', function(req,res){
+  res.render('done');
+});
+
+//get verify page
+router.get('/verify', function(req,res){
+  res.render('verify');
+});
+
 module.exports = router;
